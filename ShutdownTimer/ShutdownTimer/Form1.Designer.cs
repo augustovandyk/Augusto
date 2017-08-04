8ed13e729a859d0a33a8da15894e5cad5dd5f4ff
Augusto <augustovandyk@gmail.com>
2017-08-05 00:19:26 +0200

    Added timer and its code, displaying remaining time is still left to do

--- ShutdownTimer/ShutdownTimer/Form1.Designer.cs
+++ ShutdownTimer/ShutdownTimer/Form1.Designer.cs
@@ -34,11 +34,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lblMinutes = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -59,7 +60,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(132, 136);
+            this.button1.Location = new System.Drawing.Point(151, 120);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 23);
             this.button1.TabIndex = 2;
@@ -70,77 +71,87 @@
             // timer1
             // 
             this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 18);
+            this.label2.Location = new System.Drawing.Point(39, 27);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Hours:";
             // 
-            // label3
+            // lblHours
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(99, 23);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.lblHours.AutoSize = true;
+            this.lblHours.Location = new System.Drawing.Point(83, 27);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(0, 13);
+            this.lblHours.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(157, 27);
+            this.label4.Location = new System.Drawing.Point(108, 27);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(47, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "Minutes:";
             // 
-            // label5
+            // lblMinutes
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(234, 27);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "label5";
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Location = new System.Drawing.Point(161, 27);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(0, 13);
+            this.lblMinutes.TabIndex = 6;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(290, 23);
+            this.label6.Location = new System.Drawing.Point(183, 27);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 13);
             this.label6.TabIndex = 7;
             this.label6.Text = "Seconds:";
             // 
-            // label7
+            // lblSeconds
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(342, 23);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
-            this.label7.TabIndex = 8;
-            this.label7.Text = "label7";
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Location = new System.Drawing.Point(241, 27);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(0, 13);
+            this.lblSeconds.TabIndex = 8;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(260, 120);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(407, 212);
-            this.Controls.Add(this.label7);
+            this.ClientSize = new System.Drawing.Size(407, 174);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblSeconds);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMinutes);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblHours);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Shutdown Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,11 +164,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHours;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
 
--- ShutdownTimer/ShutdownTimer/Form1.cs
+++ ShutdownTimer/ShutdownTimer/Form1.cs
@@ -2,23 +2,51 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ShutdownTimer
 {
     public partial class Form1 : Form
     {
+        // Global variables
+        int iCount, iInput;
+        ProcessStartInfo startInfo;
+
         public Form1()
         {
             InitializeComponent();
+            iCount = 0; iInput = 0;
+            startInfo = new ProcessStartInfo("shutdown.exe", "-s");// create object to execute shutdown.exe
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            timer1.Start();
+            iInput = Convert.ToInt16(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            iCount +=  1;// increment time variable by one to keep record of time elapsed
+            if (iCount == iInput)
+                Process.Start(startInfo);
+            
 
         }
     }
